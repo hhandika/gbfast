026cfbfc8b7605da98bfaf2766baed27d4f1f83c
Heru Handika <herubiolog@gmail.com>
2025-03-26 13:55:54 -0500

    Add prefix for id.

--- converter.py
+++ converter.py
@@ -3,7 +3,7 @@
 from Bio import SeqIO
 import argparse
 
-def convert_gb_to_fasta(gb_file, fasta_file):
+def convert_gb_to_fasta(gb_file, fasta_file, id_prefix):
     with open(fasta_file, 'w') as output_handle:
         for seq_record in SeqIO.parse(gb_file, "genbank"):
             organism = seq_record.annotations.get("organism", "unknown_organism").replace(" ", "_")
@@ -12,20 +12,22 @@ def convert_gb_to_fasta(gb_file, fasta_file):
                 if feature.type == "CDS":
                     gene_seq = feature.extract(seq_record.seq)
                     gene_id = feature.qualifiers.get("gene", feature.qualifiers.get("product", ["unknown_gene"]))[0]
-                    output_handle.write(f">{organism}_{genebank_id}_{gene_id}\n{gene_seq}\n")
+                    sequence_id = f"{organism}_{genebank_id}_{id_prefix}-{gene_id}"
+                    output_handle.write(f">{sequence_id}\n{gene_seq}\n")
                 if feature.type == "rRNA":
                     gene_seq = feature.extract(seq_record.seq)
                     gene_id = feature.qualifiers.get("gene", feature.qualifiers.get("product", ["unknown_gene"]))[0]
-                    output_handle.write(f">{organism}_{genebank_id}_{gene_id}\n{gene_seq}\n")
-
+                    sequence_id = f"{organism}_{genebank_id}_{id_prefix}-{gene_id}"
+                    output_handle.write(f">{sequence_id}\n{gene_seq}\n")
 
 def main():
     parser = argparse.ArgumentParser(description="Convert GenBank file to FASTA format.")
     parser.add_argument("-g", "--gb", help="Path to the input GenBank file.", required=True)
     parser.add_argument("-o", "--output", help="Path to the output FASTA file.", required=True)
+    parser.add_argument("--id-prefix", help="Prefix for the sequence ID.", default="")
     args = parser.parse_args()
 
-    convert_gb_to_fasta(args.gb, args.output)
+    convert_gb_to_fasta(args.gb, args.output, args.id_prefix)
 
 
 if __name__ == '__main__':
